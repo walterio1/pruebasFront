e7dd6bc09d7c651cb709b19b2782de3bdfb5adbe
Walter Scherk <walterscherk@yahoo.com>
2023-05-01 23:48:42 +0200

    creación de Index2 y cruz en centro

--- src/funciones1.js
+++ src/funciones1.js
@@ -84,5 +84,33 @@ function pintaFichasIzquierda(fichasSerpiente, posIndice, ubicacionX, ubicacionY
     } while ((ubicacionX > 0.27) && (posIndice < fichasSerpiente.length));
     return [posIndice, ubicacionX];
 }   // cierra la función pintaFichasIzquierda
+
+function pintaFichaSubeBaja(posIndice, fichasSerpiente, ubicacionX, ubicacionY, fontRelativo, ratio,
+    para1, para2, para3, para4, para5, para6, para7) {
+
+if (posIndice < fichasSerpiente.length) {  //  aún queda una o más fichas 
+    laFicha = giraFicha(fichasSerpiente[posIndice]);
+    if (laFicha[0] === laFicha[2]) {   // es un doble
+        ubicacionX += para1*fontRelativo;
+        ubicacionY += para2*fontRelativo/ratio;
+        fichaVisual = hallaStringFicha(laFicha, "h");
+        colocaString(fichaVisual, fontRelativo, ubicacionX, ubicacionY);
+        ubicacionY += para3*fontRelativo/ratio;
+    }  // cierra el if de laFicha[0]
+    else {   // es una mixta
+        posicion = "v"; 
+        ubicacionY += para4*fontRelativo/ratio;
+        ubicacionX += para5*fontRelativo;
+        fichaVisual = hallaStringFicha(laFicha, "v");
+        colocaString(fichaVisual, fontRelativo, ubicacionX, ubicacionY);
+        ubicacionY += para6*fontRelativo/ratio;
+    }   // cierra el else
+    ubicacionX += para7*fontRelativo;
+    posIndice += 1;
+}   // cierre del if posIndice de 1ª ficha que sube a la derecha
+return [posIndice, ubicacionX, ubicacionY];
+}   // cierre de la función pintaFichaSubeBaja
+
+
     
     
